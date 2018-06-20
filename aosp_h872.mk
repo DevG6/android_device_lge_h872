f4ad017361643f7e274db92de1ec291e54ac0bbb
Eliminater74 <eliminater74@gmail.com>
2018-06-20 11:19:08 -0400

    [PF] Now lets Make for PureFusionOS:
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>

--- aosp_h872.mk
+++ aosp_h872.mk
@@ -1,14 +1,39 @@
+# Copyright 2018 Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 $(call inherit-product, device/lge/h872/full_h872.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/aosp/common.mk)
+IS_ARM64 := true
+
+# Include Official OTA Package
+# WITH_OFFICIALOTA := true
+
+# Support Beta OTA Opt-in (Requires additional ota xml)
+BETA_OTA := true
+
+TARGET_TRICK_SELINUX := true
+
+# Include pure telephony configuration
+$(call inherit-product, vendor/pure/configs/pure_phone.mk)
 
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Overlays (inherit after vendor/cm to ensure we override it)
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
-PRODUCT_NAME := aosp_h872
+PRODUCT_NAME := h872
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g6" \
