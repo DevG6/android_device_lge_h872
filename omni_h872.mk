5df7e01533c734d6da57f09dd423e19561b8d8e7
Eliminater74 <eliminater74@gmail.com>
2018-08-13 20:12:54 -0400

    [twrp] [h872] Fixed Up Device Tree:
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>

--- omni_h872.mk
+++ omni_h872.mk
@@ -13,19 +13,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+# common g6
+$(call inherit-product, device/lge/g6-common/g6.mk)
 
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-
-# The gps config appropriate for this device
-$(call inherit-product-if-exists, device/common/gps/gps_us_supl.mk)
-
 $(call inherit-product-if-exists, vendor/lge/h872/h872-vendor.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/board/generic_arm64/device.mk)
-# Inherit from our custom product configuration
-$(call inherit-product, vendor/omni/config/common.mk)
 
 ifeq ($(TARGET_PREBUILT_KERNEL),)
 	LOCAL_KERNEL := device/lge/h872/kernel
@@ -36,27 +29,6 @@ endif
 PRODUCT_COPY_FILES += \
     $(LOCAL_KERNEL):kernel
 
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
-
-PRODUCT_PACKAGES += \
-	adbd
-
-# Boot animation
-TARGET_SCREEN_HEIGHT := 2880
-TARGET_SCREEN_WIDTH := 1440
-
-# Define time zone data path
-ifneq ($(wildcard bionic/libc/zoneinfo),)
-    TZDATAPATH := bionic/libc/zoneinfo
-else ifneq ($(wildcard system/timezone),)
-    TZDATAPATH := system/timezone/output_data/iana
-endif
-
-# Time Zone data for Recovery
-ifdef TZDATAPATH
-PRODUCT_COPY_FILES += \
-    $(TZDATAPATH)/tzdata:recovery/root/system/usr/share/zoneinfo/tzdata
-endif
 
 PRODUCT_DEVICE := h872
 PRODUCT_NAME := omni_h872
